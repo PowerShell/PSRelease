acba4d2bd16e3fa3f6e78c727b7d720046c2ffc2
Travis Plunk <travis.plunk@microsoft.com>
2017-03-08 14:17:47 -0800

    Fix packaging issue due to missing crossgen

--- Images/microsoft_powershell_opensuse13.2/PowerShellPackage.ps1
+++ Images/microsoft_powershell_opensuse13.2/PowerShellPackage.ps1
@@ -21,10 +21,9 @@ $output = Split-Path -Parent (Get-PSOutput -Options (New-PSOptions -Publish))
 #TODO update to use crossgen
 Start-PSBuild -Runtime 'opensuse.13.2-x64' -PSModuleRestore -Publish
 
-
-Start-PSPackage
-Start-PSPackage -Type AppImage
-
+#TODO update with Start-PSPackage once we crossGen
+$Version = (git --git-dir="$PWD/.git" describe) -Replace '^v'
+New-UnixPackage -Type rpm -PackageSourcePath $output -Name powershell -Version $Version
 Pop-Location
 
 $linuxPackages = Get-ChildItem "$location/powershell*" -Include *.deb,*.rpm,*.AppImage
