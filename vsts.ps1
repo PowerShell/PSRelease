5cadf1036170471c46b0171b22b04af4bfeb646a
Travis Plunk <travis.plunk@microsoft.com>
2017-06-03 17:44:50 -0700

    add module to manage builds make docker cleanup more universal re-enable taskstate instrumentation for linux make report task status correctly print docker result to host Add support for AppImage output the destination for the packages have container write directly into bin folder default $isWindows to true if it doesn't exist Add ubuntu14.04 set repo location based on image type Add linux output support support ubuntu 16.04 write docker_host to results use a unique destination folder each build improve logging in build module Add publish artifacts function add opensuse 42.1 support add cleanup code fix publish to match current package destination only prune images on windows On Linux use --rm (more reliable, but -v is not reliable on windows) make script compatible with older version of docker on linux vms declare Dependant variable before used prune container instead of --rm Add Centos support and start using docker cp for windows Write errors using VSTS tasks for Linux in build module aggressively cleanup containers only cleanup files set git path make sure failures when calling docker are logged Add vsts task state also print docker result to host

--- vsts.ps1
+++ vsts.ps1
@@ -3,28 +3,26 @@ param(
     [Parameter(ParameterSetName='BuildContainer',Mandatory=$true)]
     [Switch]
     $BuildContainer,
+
     [Parameter(ParameterSetName='Build',Mandatory=$true)]
     [Switch]
     $Build,
+
     [Parameter(ParameterSetName='PublishArtifacts',Mandatory=$true)]
     [Switch]
     $PublishArtifacts,
-    [Parameter(ParameterSetName='ClonePowerShell',Mandatory=$true)]
-    [Switch]
-    $ClonePowerShell,
 
-    [Parameter(ParameterSetName='AgentCleanup',Mandatory=$true)]
-    [Switch]
-    $AgentCleanup,
     # end switches
     # begin parameters
     [Parameter(ParameterSetName='BuildContainer',Mandatory=$true)]
     [Parameter(ParameterSetName='Build',Mandatory=$true)]
     [string]
     $Image,
+
     [Parameter(ParameterSetName='Build',Mandatory=$true)]
     [String]
     $Runtime,
+
     [Parameter(ParameterSetName='Build')]
     [switch]
     $AppImage
@@ -44,16 +42,8 @@ try
             Invoke-PSDockerBuild -image $Image -Runtime $Runtime -AppImage:$AppImage.IsPresent
         }
 
-        'ClonePowerShell' {
-            Invoke-PSClonePowerShell -location $ENV:Build_SourcesDirectory\powershell
-        }
-
         'PublishArtifacts' {
             Invoke-PSPublishBuildArtifact
-        }
-
-        'AgentCleanup' {
-            Invoke-PSVstsCleanup
         }
 
         default {
