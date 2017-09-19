06db3d59e08abb6787c70201345990254165f08a
Travis Plunk <travis.plunk@microsoft.com>
2017-09-19 10:50:29 -0700

    Change destination to a temp folder - psrelease update module imports refactor dockerbased build into new module - psrelease fix import-module commands

--- psrelease.psm1
+++ psrelease.psm1
@@ -1,81 +1,7 @@
-# VSTS task states: Succeeded|SucceededWithIssues|Failed|Cancelled|Skipped
-$succeededStateName = 'Succeeded'
-$warningStateName = 'SucceededWithIssues'
-$errorStateName = 'Failed'
 
-# store the current state used by *-VstsTaskState and Write-VstsMessage
-$script:taskstate = $succeededStateName
-
-# on pre-6.0 PowerShell $IsWindows doesn't exist, but those are always windows
-if($IsWindows -eq $null)
-{
-    $IsWindows = $true
-}
-
-function Clear-VstsTaskState
-{
-    $script:taskstate = $succeededStateName
-}
-
-# Get the full images name based on the parameter
-function Get-BuildImageName
-{
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        $image
-    )
-
-    return "microsoft/powershell:psrelease-$image"
-}
-
-# Get the destination for the build
-if($IsWindows)
-{
-    $destination=$env:AGENT_WORKFOLDER
-}
-else {
-    $destination=$env:BUILD_BINARIESDIRECTORY
-}
-
-function Get-Destination
-{
-    [cmdletbinding(DefaultParameterSetName='default')]
-    param(
-        [Parameter(ParameterSetName='full',Mandatory=$true)]
-        [switch]$Full
-    )
-
-    if($env:BUILD_BINARIESDIRECTORY)
-    {
-        if($full.IsPresent)
-        {
-            return $env:BUILD_BINARIESDIRECTORY
-        }
-        # Docker cannot mount BUILD_BINARIESDIRECTORY
-        return $destination
-    }
-    return $env:TEMP
-}
-
-function Get-TempFolder
-{
-    $tempPath = [System.IO.Path]::GetTempPath()
-    # Use the agent temp on VSTS which is cleanup between builds (the user temp is not)
-    if($env:AGENT_TEMPDIRECTORY)
-    {
-        $tempPath = $env:AGENT_TEMPDIRECTORY
-    }
-
-    $tempFolder = Join-Path -Path $tempPath -ChildPath ([System.IO.Path]::GetRandomFileName())
-    if(!(test-path $tempFolder))
-    {
-        $null = New-Item -Path $tempFolder -ItemType Directory
-    }
-
-    return $tempFolder
-}
-
+Import-Module "$PSScriptRoot\vstsBuild"
+Import-Module "$PSScriptRoot\dockerBaseBuild"
+Import-Module "$PSScriptRoot\dockerBasedBuild\dockerBasedBuild.common.psm1"
 # Builds a Docker container for an image
 function Invoke-PSBuildContainer
 {
@@ -93,29 +19,217 @@ function Invoke-PSBuildContainer
         $dockerFilePath = Join-Path -Path $imagePath -ChildPath Dockerfile
         
         $imageName = Get-BuildImageName -image $image
+        $buildParameters = @{
+            DockerFilePath = $dockerFilePath
+            ImageName = $imageName
+        }
 
         # for linux images, use the common folder for the context
         if($image -ne 'windowsservercore')
         {
             $contextPath = Get-TempFolder
-            $genericFilesPath = Join-Path (Join-Path (join-path $PSScriptRoot -ChildPath 'Images')  -ChildPath "GenericLinuxFiles") -ChildPath '*'
-            Copy-Item -Path $genericFilesPath -Destination $contextPath
-            Copy-Item -Path $dockerFilePath -Destination $contextPath
-        }
-        else 
-        {
-            $contextPath = $imagePath
+            $buildParameters['AdditionalContextFiles']=@(genericFilesPath)
+            $buildParameters['ContextPath']=$contextPath
         }
 
-        # always log docker host information to allow troubleshooting issues with docker
-        Write-Verbose "Docker_host: $env:DOCKER_HOST" -Verbose
-        $null = Invoke-Docker -command build -params '--force-rm', '--tag', $imageName, $contextPath
-        $null = Invoke-Docker -command images -params 'ls' -FailureAction warning
+        Invoke-BuildContainer @buildParameters
 
         if($image -ne 'windowsservercore')
         {
             remove-item $contextPath -Recurse -Force -ErrorAction SilentlyContinue
         }
+    } 
+    catch
+    {
+        Write-VstsError $_
+    }
+}
+
+# Builds a Docker container for an image
+function Invoke-BuildContainer
+{
+    [cmdletbinding(DefaultParameterSetName='default')]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $DockerFilePath,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ImageName,
+
+        [string[]]
+        $AdditionalContextFiles,
+
+        [string]
+        $ContextPath,
+
+        [Parameter(ParameterSetName='addrepo',Mandatory)]
+        [switch]
+        $AddRepo,
+
+        [Parameter(ParameterSetName='addrepo',Mandatory)]
+        [string]
+        $RepoLocation,
+
+        [Parameter(ParameterSetName='addrepo',Mandatory)]
+        [string]
+        $ContainerRepoLocation
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    try {
+        $runtimeContextPath = $null
+        if($ContextPath)
+        {
+            $runtimeContextPath = $ContextPath
+            Copy-Item -Path $dockerFilePath -Destination $contextPath
+            foreach($additionalContextFile in $AdditionalContextFiles)
+            {
+                Copy-Item -Path $additionalContextFile -Destination $contextPath 
+            }
+        }
+        else 
+        {
+            $runtimeContextPath = Split-Path -Path $DockerFilePath    
+        }
+
+        $dockerBuildImageName = $ImageName
+        if($AddRepo)
+        {
+            $dockerBuildImageName = $ImageName+'-without-repo'
+        }
+
+        # always log docker host information to allow troubleshooting issues with docker
+        log "Docker_host: $env:DOCKER_HOST"
+        # Build the container, pulling to ensure we have the newest base image
+        $null = Invoke-Docker -command build -params '--pull', '--tag', $dockerBuildImageName, $runtimeContextPath
+
+        if($contextPath)
+        {
+            remove-item $contextPath -Recurse -Force -ErrorAction SilentlyContinue
+        }
+
+        if($AddRepo.IsPresent)
+        {
+            $dockerContainerName = 'pswsctemp'
+
+            $repoFolderName = 'repolink'
+
+            $dockerBuildFolder = Get-TempFolder
+
+            $repoPath = Join-Path -Path $dockerBuildFolder -ChildPath $repoFolderName
+
+            try 
+            {
+                $addRepoDockerFilePath = Join-Path -Path $dockerBuildFolder -ChildPath 'Dockerfile'
+                
+                # TODO: redo using symbolic links, but hit many isssue using them.
+                log "Copying repo from: $RepoLocation to: $RepoPath"
+                Copy-item -path $RepoLocation -Destination $RepoPath -Recurse
+                
+                $psreleaseStrings.dockerfile -f $dockerBuildImageName, $repoFolderName, $ContainerRepoLocation | Out-File -FilePath $addRepoDockerFilePath -Encoding ascii -Force
+
+                $null = Invoke-Docker -command build -params '--tag', $ImageName, $dockerBuildFolder
+            }
+            finally
+            {
+                if(Test-Path $dockerBuildFolder)
+                {
+                    Remove-Item -Path $dockerBuildFolder -Recurse -Force -ErrorAction SilentlyContinue
+                }
+            }
+        }
+    } 
+    catch
+    {
+        Write-VstsError $_
+    }
+}
+
+# Builds a Docker container
+function Invoke-DockerBuild
+{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ImageName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $RepoLocation,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ContainerRepoLocation,
+
+        [ValidateNotNullOrEmpty()]
+        [string] $Destination = (Get-Destination),
+
+        [string[]] $DockerOptions,
+
+        [string] $BuildCommand,
+
+        [hashtable] $Parameters
+    )
+    $runtimeParameters = $Parameters.Clone()
+    $runtimeParameters['RepoDestinationPath']=$BuildData.RepoDestinationPath
+
+    $ErrorActionPreference = 'Stop'
+
+    try {
+        
+        if($IsWindows)
+        {
+            $outputFolder = 'C:\out'
+        }
+        else 
+        {
+            $outputFolder = '/mnt'
+        }
+        $runtimeParameters['DockerVolume']=$outputFolder
+        
+        if(!(Test-Path $destination))
+        {
+            $null = New-Item -Path $destination -ItemType Directory -Force
+        }
+
+        $dockerContainerName = 'pswscbuild'       
+
+        $params = @('-i', '--name', $dockerContainerName)
+
+        if($DockerOptions)
+        {
+            $params += $DockerOptions
+        }
+
+        $params += $imageName
+        $runtimeBuildCommand = [System.Text.StringBuilder]::new($BuildCommand)
+        foreach($key in $runtimeParameters.Keys)
+        {
+            $token = "_${key}_"
+            $value = $runtimeParameters.$key
+            $null = $runtimeBuildCommand.Replace($token,$value)
+        }
+
+        $runtimeBuildCommandString = $runtimeBuildCommand.ToString()
+        foreach($param in $runtimeBuildCommandString -split ' ')
+        {
+            $params += $param
+        }
+
+        # Cleanup any existing container from previous runs before starting
+        # Ignore failure, because it will fail if they do not exist
+        Remove-Container -FailureAction ignore
+
+        $null = Invoke-Docker -command run -params $params
+
+        log "coping artifacts from container" -verbose
+        $null = Invoke-Docker -command 'container', 'cp' -params "${dockerContainerName}:$outputFolder", $Destination
+
+        # We are done with the containers, remove them
+        Remove-Container 
     } 
     catch
     {
@@ -275,14 +389,23 @@ function Invoke-Docker
     $ErrorActionPreference = 'Continue'
 
     # Log how we are running docker for troubleshooting issues
-    Write-Verbose "Running docker $command $params" -Verbose
+    log "Running docker $command $params"
+    $dockerErrors = $null
     if($SupressHostOutput.IsPresent)
     {
         $result = &'docker' $command $params 2>&1
     }
     else 
     {
-        &'docker' $command $params 2>&1 | Tee-Object -Variable result -ErrorAction SilentlyContinue | Out-String -Stream -ErrorAction SilentlyContinue | Write-Host -ErrorAction SilentlyContinue
+        &'docker' $command $params 2>&1 | Tee-Object -Variable result -ErrorAction SilentlyContinue -ErrorVariable dockerErrors | Out-String -Stream -ErrorAction SilentlyContinue | Write-Host -ErrorAction SilentlyContinue
+    }
+
+    if($dockerErrors -and $FailureAction -ne 'ignore')
+    {
+        foreach($error in $dockerErrors)
+        {
+            Write-VstsError -Error $error -Type $FailureAction
+        }
     }
 
     $dockerExitCode = $LASTEXITCODE
@@ -337,25 +460,23 @@ function Remove-Container
 function Invoke-PSPublishBuildArtifact
 {
     $ErrorActionPreference = 'Continue'
-    if($env:BUILD_BINARIESDIRECTORY)
+    $filter = Join-Path -Path (Get-Destination) -ChildPath '*'
+    log "Publishing artifacts: $filter"
+
+    # In VSTS, publish artifacts appropriately
+    $files = Get-ChildItem -Path $filter -Recurse | Select-Object -ExpandProperty FullName
+
+    foreach($fileName in $files)
     {
-        $filter = Join-Path -Path (Get-Destination -Full) -ChildPath '*'
+        $leafFileName = $(Split-path -Path $FileName -Leaf)
 
-        # In VSTS, publish artifacts appropriately
-        $files = Get-ChildItem -Path $filter -Recurse | Select-Object -ExpandProperty FullName
-
-        foreach($fileName in $files)
+        $extension = [System.io.path]::GetExtension($fileName)
+        if($extension -ieq '.zip')
         {
-            $leafFileName = $(Split-path -Path $FileName -Leaf)
-
-            $extension = [System.io.path]::GetExtension($fileName)
-            if($extension -ieq '.zip')
-            {
-                Expand-Archive -Path $fileName -DestinationPath (Join-Path $env:Build_StagingDirectory -ChildPath $leafFileName)
-            }
-
-            Write-Host "##vso[artifact.upload containerfolder=results;artifactname=$leafFileName]$FileName"
+            Expand-Archive -Path $fileName -DestinationPath (Join-Path $env:Build_StagingDirectory -ChildPath $leafFileName)
         }
+
+        Write-Host "##vso[artifact.upload containerfolder=results;artifactname=$leafFileName]$FileName"
     }
 }
 
@@ -363,7 +484,9 @@ function Write-VstsError {
     param(
         [Parameter(Mandatory=$true)]
         [Object]
-        $Error
+        $Error,
+        [ValidateSet("error","warning")]
+        $Type = 'error'
     )
 
     $message = [string]::Empty
@@ -384,12 +507,12 @@ function Write-VstsError {
         default
         {
             # Log any unknown error types we get so  we can improve logging.
-            Write-Verbose "errorType: $errorType" -Verbose
+            log "errorType: $errorType"
             $message =  $Error.ToString()
         }
     }
     $message.Split($newLine) | ForEach-Object {
-        Write-VstsMessage -type error -message $PSItem
+        Write-VstsMessage -type $Type -message $PSItem
     }
 }
 
@@ -422,6 +545,37 @@ function Write-VstsTaskState
     Write-Host "##vso[task.complete result=$script:taskstate;]DONE"
 }
 
+# this function wraps native command Execution
+# for more information, read https://mnaoumov.wordpress.com/2015/01/11/execution-of-external-commands-in-powershell-done-right/
+function script:Start-NativeExecution([scriptblock]$sb, [switch]$IgnoreExitcode)
+{
+    log "Running $($sb.ToString())"
+    $backupEAP = $script:ErrorActionPreference
+    $script:ErrorActionPreference = "Continue"
+    try {
+        & $sb
+        # note, if $sb doesn't have a native invocation, $LASTEXITCODE will
+        # point to the obsolete value
+        if ($LASTEXITCODE -ne 0 -and -not $IgnoreExitcode) {
+            throw "Execution of {$sb} failed with exit code $LASTEXITCODE"
+        }
+    } finally {
+        $script:ErrorActionPreference = $backupEAP
+    }
+}
+
+function script:log([string]$message) {
+    Write-Host -Foreground Green $message
+    #reset colors for older package to at return to default after error message on a compilation error
+    [console]::ResetColor()
+}
+
+function script:logerror([string]$message) {
+    Write-Host -Foreground Red $message
+    #reset colors for older package to at return to default after error message on a compilation error
+    [console]::ResetColor()
+}
+
 Export-ModuleMember @(
     'Invoke-PSBuildContainer'
     'Invoke-PSDockerBuild'
@@ -429,4 +583,5 @@ Export-ModuleMember @(
     'Write-VstsError'
     'Clear-VstsTaskState'
     'Write-VstsTaskState'
+    'Invoke-Build'
 )
