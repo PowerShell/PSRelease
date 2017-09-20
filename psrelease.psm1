fd7e8cf170e80844d810c1ab5acc2224a9620b75
Travis Plunk <travis.plunk@microsoft.com>
2017-09-20 14:04:57 -0700

    Docker based build (#18)
    * remove unused jenkins files
    * seperate VSTS commands into a new module
    * refactor dockerbased build into new module

--- psrelease.psm1
+++ psrelease.psm1
@@ -1,79 +1,11 @@
-# VSTS task states: Succeeded|SucceededWithIssues|Failed|Cancelled|Skipped
-$succeededStateName = 'Succeeded'
-$warningStateName = 'SucceededWithIssues'
-$errorStateName = 'Failed'
-
-# store the current state used by *-VstsTaskState and Write-VstsMessage
-$script:taskstate = $succeededStateName
+Import-Module "$PSScriptRoot/vstsBuild" -Scope Global
+Import-Module "$PSScriptRoot/dockerBasedBuild"
+Import-Module "$PSScriptRoot/dockerBasedBuild/dockerBasedBuild.common.psm1"
 
 # on pre-6.0 PowerShell $IsWindows doesn't exist, but those are always windows
 if($IsWindows -eq $null)
 {
     $IsWindows = $true
-}
-
-function Clear-VstsTaskState
-{
-    $script:taskstate = $succeededStateName
-}
-
-# Get the full images name based on the parameter
-function Get-BuildImageName
-{
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]
-        $image
-    )
-
-    return "microsoft/powershell:psrelease-$image"
-}
-
-# Get the destination for the build
-if($IsWindows)
-{
-    $destination=$env:AGENT_WORKFOLDER
-}
-else {
-    $destination=$env:BUILD_BINARIESDIRECTORY
-}
-
-function Get-Destination
-{
-    [cmdletbinding(DefaultParameterSetName='default')]
-    param(
-        [Parameter(ParameterSetName='full',Mandatory=$true)]
-        [switch]$Full
-    )
-
-    if($env:BUILD_BINARIESDIRECTORY)
-    {
-        if($full.IsPresent)
-        {
-            return $env:BUILD_BINARIESDIRECTORY
-        }
-        # Docker cannot mount BUILD_BINARIESDIRECTORY
-        return $destination
-    }
-    return $env:TEMP
-}
-
-function Get-TempFolder
-{
-    $tempPath = [System.IO.Path]::GetTempPath()
-    # Use the agent temp on VSTS which is cleanup between builds (the user temp is not)
-    if($env:AGENT_TEMPDIRECTORY)
-    {
-        $tempPath = $env:AGENT_TEMPDIRECTORY
-    }
-
-    $tempFolder = Join-Path -Path $tempPath -ChildPath ([System.IO.Path]::GetRandomFileName())
-    if(!(test-path $tempFolder))
-    {
-        $null = New-Item -Path $tempFolder -ItemType Directory
-    }
-
-    return $tempFolder
 }
 
 # Builds a Docker container for an image
@@ -93,24 +25,20 @@ function Invoke-PSBuildContainer
         $dockerFilePath = Join-Path -Path $imagePath -ChildPath Dockerfile
         
         $imageName = Get-BuildImageName -image $image
+        $buildParameters = @{
+            DockerFilePath = $dockerFilePath
+            ImageName = $imageName
+        }
 
         # for linux images, use the common folder for the context
         if($image -ne 'windowsservercore')
         {
             $contextPath = Get-TempFolder
-            $genericFilesPath = Join-Path (Join-Path (join-path $PSScriptRoot -ChildPath 'Images')  -ChildPath "GenericLinuxFiles") -ChildPath '*'
-            Copy-Item -Path $genericFilesPath -Destination $contextPath
-            Copy-Item -Path $dockerFilePath -Destination $contextPath
-        }
-        else 
-        {
-            $contextPath = $imagePath
+            $buildParameters['AdditionalContextFiles']=@('./Images/GenericLinuxFiles/PowerShellPackage.ps1')
+            $buildParameters['ContextPath']=$contextPath
         }
 
-        # always log docker host information to allow troubleshooting issues with docker
-        Write-Verbose "Docker_host: $env:DOCKER_HOST" -Verbose
-        $null = Invoke-Docker -command build -params '--force-rm', '--tag', $imageName, $contextPath
-        $null = Invoke-Docker -command images -params 'ls' -FailureAction warning
+        Invoke-BuildContainer @buildParameters
 
         if($image -ne 'windowsservercore')
         {
@@ -122,6 +50,7 @@ function Invoke-PSBuildContainer
         Write-VstsError $_
     }
 }
+
 
 # Builds PowerShell in a Docker container
 function Invoke-PSDockerBuild
@@ -181,7 +110,7 @@ function Invoke-PSDockerBuild
             $null = New-Item -Path $destination -ItemType Directory -Force
         }
 
-        $dockerContainerName = 'pswscbuild'
+        $dockerContainerName = 'pswscbuildlegacy'
 
         $volumeMapping = "${destination}:$outputFolder"
         $params = @('-i')
@@ -244,9 +173,11 @@ function Invoke-PSDockerBuild
 
         if($IsWindows)
         {
-            $null = Invoke-Docker -command 'container', 'cp' -params "${dockerContainerName}:$outputFolder", $env:BUILD_BINARIESDIRECTORY
+            $null = Invoke-Docker -command 'container', 'cp' -params "${dockerContainerName}:$outputFolder", $destination
             Remove-Container 
         }
+
+        Invoke-VstsPublishBuildArtifact -ArtifactPath $destination
     } 
     catch
     {
@@ -254,179 +185,8 @@ function Invoke-PSDockerBuild
     }
 }
 
-# Call Docker with appropriate result checks
-function Invoke-Docker 
-{
-    param(
-        [Parameter(Mandatory=$true)]
-        [string[]]
-        $Command,
-        [ValidateSet("error","warning",'ignore')]
-        $FailureAction = 'error',
-        [Parameter(Mandatory=$true)]
-        [string[]]
-        $Params,
-        [switch]
-        $PassThru,
-        [switch]
-        $SupressHostOutput
-    )
-
-    $ErrorActionPreference = 'Continue'
-
-    # Log how we are running docker for troubleshooting issues
-    Write-Verbose "Running docker $command $params" -Verbose
-    if($SupressHostOutput.IsPresent)
-    {
-        $result = &'docker' $command $params 2>&1
-    }
-    else 
-    {
-        &'docker' $command $params 2>&1 | Tee-Object -Variable result -ErrorAction SilentlyContinue | Out-String -Stream -ErrorAction SilentlyContinue | Write-Host -ErrorAction SilentlyContinue
-    }
-
-    $dockerExitCode = $LASTEXITCODE
-    if($PassThru.IsPresent)
-    {
-        return $result
-    }
-    elseif($dockerExitCode -ne 0 -and $FailureAction -eq 'error')
-    {
-        Write-VstsMessage -type error -message "docker $command failed with: $result"
-        return $false
-    }
-    elseif($dockerExitCode -ne 0 -and $FailureAction -eq 'warning')
-    {
-        Write-VstsMessage -type warning -message "docker $command failed with: $result"
-        return $false
-    }
-    elseif($dockerExitCode -ne 0)
-    {
-        return $false
-    }
-    
-    return $true
-}
-
-function Remove-Container
-{
-    param(
-        [ValidateSet('warning','ignore')]
-        $FailureAction = 'warning'
-    )
-
-    $commonDockerParams = @{
-        FailureAction = $FailureAction
-        SupressHostOutput = $true
-    }
-
-    # stop all running containers
-    Invoke-Docker -Command 'ps' -Params '--format', '{{ json .}}' @commonDockerParams -PassThru | 
-        Where-Object {$_ -ne $null} |
-        ConvertFrom-Json | 
-        Where-Object { $null = Invoke-Docker -Command stop -Params $_.Names  @commonDockerParams} 
-
-    # remove all containers
-    Invoke-Docker -Command 'ps' -Params '--format', '{{ json .}}', '--all' @commonDockerParams -PassThru | 
-        Where-Object {$_ -ne $null} |
-        ConvertFrom-Json | 
-        Where-Object { $null = Invoke-Docker -Command rm -Params $_.Names  @commonDockerParams} 
-} 
-
-# Publishes build artifacts 
-function Invoke-PSPublishBuildArtifact
-{
-    $ErrorActionPreference = 'Continue'
-    if($env:BUILD_BINARIESDIRECTORY)
-    {
-        $filter = Join-Path -Path (Get-Destination -Full) -ChildPath '*'
-
-        # In VSTS, publish artifacts appropriately
-        $files = Get-ChildItem -Path $filter -Recurse | Select-Object -ExpandProperty FullName
-
-        foreach($fileName in $files)
-        {
-            $leafFileName = $(Split-path -Path $FileName -Leaf)
-
-            $extension = [System.io.path]::GetExtension($fileName)
-            if($extension -ieq '.zip')
-            {
-                Expand-Archive -Path $fileName -DestinationPath (Join-Path $env:Build_StagingDirectory -ChildPath $leafFileName)
-            }
-
-            Write-Host "##vso[artifact.upload containerfolder=results;artifactname=$leafFileName]$FileName"
-        }
-    }
-}
-
-function Write-VstsError {
-    param(
-        [Parameter(Mandatory=$true)]
-        [Object]
-        $Error
-    )
-
-    $message = [string]::Empty
-    $errorType = $Error.GetType().FullName
-    $newLine = [System.Environment]::NewLine
-    switch($errorType)
-    {
-        'System.Management.Automation.ErrorRecord'{
-            $message = "{0}{2}`t{1}" -f $Error,$Error.ScriptStackTrace,$newLine
-        }
-        'System.Management.Automation.ParseException'{
-            $message = "{0}{2}`t{1}" -f $Error,$Error.StackTrace,$newLine
-        }
-        'System.Management.Automation.Runspaces.RemotingErrorRecord'
-        {
-            $message = "{0}{2}`t{1}{2}`tOrigin: {2}" -f $Error,$Error.ScriptStackTrace,$Error.OriginInfo,$newLine
-        }
-        default
-        {
-            # Log any unknown error types we get so  we can improve logging.
-            Write-Verbose "errorType: $errorType" -Verbose
-            $message =  $Error.ToString()
-        }
-    }
-    $message.Split($newLine) | ForEach-Object {
-        Write-VstsMessage -type error -message $PSItem
-    }
-}
-
-
-function Write-VstsMessage {
-    param(
-        [ValidateSet("error","warning")]
-        $type = 'error',
-        [String]
-        $message
-    )
-
-    if($script:taskstate -ne $errorStateName -and $type -eq 'error')
-    {
-        $script:taskstate = $errorStateName
-    }
-    elseif($script:taskstate -eq $succeededStateName) {
-        $script:taskstate = $warningStateName
-    }
-
-    # See VSTS documentation at https://github.com/Microsoft/vsts-tasks/blob/master/docs/authoring/commands.md
-    # Log task message
-    Write-Host "##vso[task.logissue type=$type]$message"
-}
-
-function Write-VstsTaskState
-{
-    # See VSTS documentation at https://github.com/Microsoft/vsts-tasks/blob/master/docs/authoring/commands.md
-    # Log task state
-    Write-Host "##vso[task.complete result=$script:taskstate;]DONE"
-}
-
 Export-ModuleMember @(
     'Invoke-PSBuildContainer'
     'Invoke-PSDockerBuild'
     'Invoke-PSPublishBuildArtifact'
-    'Write-VstsError'
-    'Clear-VstsTaskState'
-    'Write-VstsTaskState'
 )
