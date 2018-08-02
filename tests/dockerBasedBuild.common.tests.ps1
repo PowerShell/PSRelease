2d83f7676c074f83353cb4523e94ecdcaf2e6bae
Travis Plunk <travis.plunk@microsoft.com>
2018-08-01 18:11:41 -0700

    Fix break if Enable Feature is missing

--- tests/dockerBasedBuild.common.tests.ps1
+++ tests/dockerBasedBuild.common.tests.ps1
@@ -9,16 +9,14 @@ $modulePath = "$PSScriptRoot\..\$vstsModuleName"
 Import-module $modulePath -force
 
 Describe "DockerBasedBuild.Common" {
-    $buildData = New-BuildData
-    $buildData.EnableFeature = @('ArtifactAsFolder')
-    $buildData.DockerFile = 'TestDockerFile'
-    $buildData.DockerImageName = 'TestImageName'
-    $buildData.RepoDestinationPath = '/test'
-    $buildData.BuildCommand = './TestBuildCommand.sh'
-
-
-    Context "Invoke-BuildInDocker" {
+    Context "Invoke-BuildInDocker -ArtifactAsFolder" {
         BeforeAll{
+            $buildData = New-BuildData
+            $buildData.EnableFeature = @('ArtifactAsFolder')
+            $buildData.DockerFile = 'TestDockerFile'
+            $buildData.DockerImageName = 'TestImageName'
+            $buildData.RepoDestinationPath = '/test'
+            $buildData.BuildCommand = './TestBuildCommand.sh'
             $emptyScriptBlock = {}
             Mock -CommandName 'New-DockerImage' -MockWith $emptyScriptBlock -Verifiable -ModuleName $moduleName
             Mock -CommandName 'Invoke-DockerBuild' -MockWith $emptyScriptBlock -Verifiable -ModuleName $moduleName
@@ -30,6 +28,28 @@ Describe "DockerBasedBuild.Common" {
             Assert-VerifiableMock
             Assert-MockCalled -CommandName 'Publish-VstsBuildArtifact' -ParameterFilter {
                 $ArtifactAsFolder -eq $true
+            } -ModuleName $moduleName
+        }
+    }
+
+    Context "Invoke-BuildInDocker" {
+        BeforeAll{
+            $buildData = New-BuildData
+            $buildData.DockerFile = 'TestDockerFile'
+            $buildData.DockerImageName = 'TestImageName'
+            $buildData.RepoDestinationPath = '/test'
+            $buildData.BuildCommand = './TestBuildCommand.sh'
+            $emptyScriptBlock = {}
+            Mock -CommandName 'New-DockerImage' -MockWith $emptyScriptBlock -Verifiable -ModuleName $moduleName
+            Mock -CommandName 'Invoke-DockerBuild' -MockWith $emptyScriptBlock -Verifiable -ModuleName $moduleName
+            Mock -CommandName 'Publish-VstsBuildArtifact' -MockWith $emptyScriptBlock -ModuleName $moduleName
+        }
+
+        It "Verify EnableFeature without ArtifactAsFolder does not trigger that feature" {
+            Invoke-BuildInDocker -BuildData $buildData -RepoLocation $TestDrive
+            Assert-VerifiableMock
+            Assert-MockCalled -CommandName 'Publish-VstsBuildArtifact' -ParameterFilter {
+                !$ArtifactAsFolder
             } -ModuleName $moduleName
         }
     }
