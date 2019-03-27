5fa6a7c89aac038aeb1fe116be2f30bb78eca2be
Travis Plunk <travis.plunk@microsoft.com>
2019-03-27 13:25:48 -0700

    Add script to install Az DevOps agent for windows (#38)

---
+++ AzDevOps/windows/install-agent.ps1
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+param(
+    [parameter(Mandatory)]
+    [string]$Url,
+    [parameter(Mandatory)]
+    [string]$Pat,
+    [parameter(Mandatory)]
+    [string]$Pool
+)
+
+$ErrorActionPreference = 'stop'
+
+$agentZipUrl = 'https://vstsagentpackage.azureedge.net/agent/2.148.2/vsts-agent-win-x64-2.148.2.zip'
+$installPsUrl = 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.ps1'
+
+Invoke-WebRequest -Uri $installPsUrl -outFile ./install-powershell.ps1
+
+$pwshDestination = Join-Path -Path $env:SystemDrive -ChildPath "pwsh"
+./install-powershell.ps1 -AddToPath -Destination $pwshDestination
+
+Invoke-WebRequest -Uri $agentZipUrl -outFile ./agent.zip
+
+$agentPath = Join-Path -Path $env:SystemDrive -ChildPath 'AzDevOpsAgent'
+Expand-Archive -Path ./agent.zip -DestinationPath $agentPath -force
+
+$workDir = Join-Path -Path $env:SystemDrive -ChildPath '1'
+$null = New-Item -ItemType Directory -Path $workDir
+
+Write-Host "Url: $Url"
+Write-Host "Pool: $pool"
+$configCmd = Join-Path -Path $agentPath -ChildPath 'config.cmd'
+& $configCmd --unattended --url $Url --auth pat --token $Pat --pool $Pool --agent $env:Computername --work $workDir --runAsService
