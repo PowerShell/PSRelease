70243b24f9835a88e1d64b701175fa205ad53b4a
Aditya Patwardhan <adityap@microsoft.com>
2019-04-18 11:35:05 -0700

    Reboot machine to populate PATH variable

--- AzDevOps/windows/install-agent.ps1
+++ AzDevOps/windows/install-agent.ps1
@@ -14,10 +14,16 @@ $ErrorActionPreference = 'stop'
 $agentZipUrl = 'https://vstsagentpackage.azureedge.net/agent/2.148.2/vsts-agent-win-x64-2.148.2.zip'
 $installPsUrl = 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.ps1'
 
-Invoke-WebRequest -Uri $installPsUrl -outFile ./install-powershell.ps1
+$pwshExe = Get-Command pwsh -ErrorAction Ignore
 
-$pwshDestination = Join-Path -Path $env:SystemDrive -ChildPath "pwsh"
-./install-powershell.ps1 -AddToPath -Destination $pwshDestination
+if (-not $pwshExe) {
+    Invoke-WebRequest -Uri $installPsUrl -outFile ./install-powershell.ps1
+    $pwshDestination = Join-Path -Path $env:SystemDrive -ChildPath "pwsh"
+    ./install-powershell.ps1 -AddToPath -Destination $pwshDestination
+
+    # A restart or logoff/logon is needed on older Windows OSes for updating the PATH environment variable
+    Restart-Computer -Force
+}
 
 Invoke-WebRequest -Uri $agentZipUrl -outFile ./agent.zip
 
